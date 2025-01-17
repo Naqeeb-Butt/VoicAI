fb7e9829aaf9561973e496ff6bfd52fdba23fc94
Naqeeb-Butt <139251654+Naqeeb-Butt@users.noreply.github.com>
2025-01-18 00:14:38 +0500

    server.js

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server is running at http://0.0.0.0:${PORT}`);
+app.listen(4000, '127.0.0.1', () => {
+    console.log('Server is running at http://127.0.0.1:4000');
 });
